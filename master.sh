5bdc5751aa785f49b9071d109f0eb77f8ac7a279
Deepankur Singh Baliyan <deepankur797@gmail.com>
2020-04-01 16:15:21 +0530

    Create master.sh
    initial master file

---
+++ master.sh
@@ -0,0 +1,31 @@
+mysqlpass=$1
+slavepass=$2
+slaveip=$3
+sudo yum install sshpass -y
+sleep 5
+cp /etc/my.cnf /etc/my.cnf.bkp
+sed -i '/\[mysqld\]/r master.txt' /etc/my.cnf
+systemctl restart mysqld
+
+sleep 10
+mysql -uroot -pAdmin987! -Bse "uninstall plugin validate_password;"
+mysql -u root -p$mysqlpass -Bse "GRANT REPLICATION SLAVE ON *.* TO 'slave_user'@'%' IDENTIFIED BY ""'\'$slavepass\'';"
+mysql -u root -p$mysqlpass -Bse "FLUSH PRIVILEGES;"
+mysql -u root -p$mysqlpass -Bse "FLUSH TABLES WITH READ LOCK;"
+mysql -u root -p$mysqlpass -Bse "SHOW MASTER STATUS;"
+
+
+file=$(mysql -u root -p$mysqlpass -Bse "SHOW MASTER STATUS;" | awk '{print $1}')
+pos=$(mysql -u root -p$mysqlpass -Bse "SHOW MASTER STATUS;" | awk '{print $2}')
+
+echo $file >>param.txt
+echo $pos >>param.txt
+mysqldump -u root -p$mysqlpass --all-databases --master-data > /root/dbdump.db
+
+mysql -u root -p$mysqlpass -Bse "UNLOCK TABLES;"
+sshpass -p '@dmin987!' scp /root/dbdump.db root@$slaveip:/root/
+
+
+firewall-cmd --zone=public --add-port=3306/tcp --permanent
+firewall-cmd --reload
+iptables-save | grep 3306
